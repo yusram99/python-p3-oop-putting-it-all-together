734734239bc7141a03453027a8becc2141949d64
Your Name <youremail@example.com>
2023-08-23 18:15:22 +0300

    commit message

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+# The following code is necessary for the tests to work properly
+if __name__ == "__main__":
+    book = Book("And Then There Were None", 272)
+    print(book.page_count)
+    print(book.title)
+    book.page_count = "not an integer"
+    book.turn_page()
+
+   
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+# class Shoe:
+#     def __init__(self, brand, size, price):
+#         self.brand = brand
+#         self.size = size
+#         self.price = price
+
+#     def __str__(self):
+#         return f"{self.brand} shoe, size {self.size}, price{self.price}"
+
+#     def is_affordable(self):
+#         return self.price <= 100
 class Shoe:
-    pass
+    def __init__(self, brand, size, price):
+        self.brand = brand
+        self._size = size
+        self.price = price
+        self._condition = "New"
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+    
+    def cobble(self):
+        print("The shoe has been repaired.")
+        self._condition = "New"
